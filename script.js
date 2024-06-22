d648b015cce0b96f8ba09793fa828af7a2492177
Rodrigofernandesdossantos <135120142+Rodrigofernandesdossantos@users.noreply.github.com>
2024-06-22 16:14:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     event.preventDefault(); // Impede o envio do formulário
 
     // Credenciais fictícias para validação
-    const validUsername = 'usuario';
+    const validUsername = 'rodrigo';
     const validPassword = 'senha123';
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
 
-    if (username ===rodrigo && password ===1234) {
+    if (username ===validUsername && password ===1234) {
         // Redireciona para o link específico após a autenticação
         window.location.href = 'https://app.powerbi.com/view?r=eyJrIjoiNjY0OTBjZWYtMjdhNC00N2Q3LWE2MjktNTg3MDQ0MTRjMWVmIiwidCI6Ijg2YzhmMDAxLTYzOTYtNDA5YS04MGQ5LWM4MGU0MDI2NzgzYyJ9';
     } else {
