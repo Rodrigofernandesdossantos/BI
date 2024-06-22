83c46a74629f57f9f2b65469cc33bcf22a7747c5
Rodrigofernandesdossantos <135120142+Rodrigofernandesdossantos@users.noreply.github.com>
2024-06-22 16:07:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
 
-    if (username === rodrigo && password === 1234) {
+    if (username ===rodrigo && password ===1234) {
         // Redireciona para o link específico após a autenticação
         window.location.href = 'https://app.powerbi.com/view?r=eyJrIjoiNjY0OTBjZWYtMjdhNC00N2Q3LWE2MjktNTg3MDQ0MTRjMWVmIiwidCI6Ijg2YzhmMDAxLTYzOTYtNDA5YS04MGQ5LWM4MGU0MDI2NzgzYyJ9';
     } else {
